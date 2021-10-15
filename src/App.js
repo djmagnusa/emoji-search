a9e88f02890924e7f92c8e514f9c284ef99ecae3
Pratush Bhandari <pratush.bh@gmail.com>
2021-10-15 17:48:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import EmojiData from './Emoji.json';
 
-function App() {
+const App = () => {
+
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+          <h1>Emoji Search</h1>
+          <input type="text" name="search" value={search} onChange={(e) => setSearch(e.target.value)} />
+      </center>       
+      {EmojiData.data}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
