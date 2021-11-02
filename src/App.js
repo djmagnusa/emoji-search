60820d361917cce31a18289e004ba3ce86e4a8cc
Pratush <pratush.bh@gmail.com>
2021-11-02 16:49:30 +0530

    Add copy to clipboard feature

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import EmojiData from './Emoji.json';
 const App = () => {
 
   const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+
 
   useEffect(() => {
-
+     const newData = EmojiData.filter(emoji => emoji.title.toLowerCase().includes(search.toLowerCase()))
+     setData(newData);
   }, [search]);
 
   return (
@@ -15,7 +18,13 @@ const App = () => {
           <h1>Emoji Search</h1>
           <input type="text" name="search" value={search} onChange={(e) => setSearch(e.target.value)} />
       </center>       
-      {EmojiData.data}
+      {data.map(emoji => <div>
+         <div class="card">
+            <div class="card-body" onClick={() => {navigator.clipboard.writeText(emoji.symbol); alert("Emoji copied to clipboard")}}>
+                {emoji.symbol} {emoji.title}
+            </div>
+         </div>
+      </div>)}  
     </div>
   )
 }
